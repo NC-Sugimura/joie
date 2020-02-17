21628129eef120091b23f69b68ff3fc19f25bdb6
NC-Sugimura <sugimura@northcode.co.jp>
2020-02-17 17:48:17 +0900

    Add 美脚ページ
    クライアント依頼によりページ新規作成

--- html/legs.php
+++ html/legs.php
@@ -1,51 +1,56 @@
-<h2>健康的で美しい足になりたい方へ</h2>
-<div>
-	<p>美しい足に憧れがあるけれど、どうしたらなれるか分からない<br>
-		デスクワークや立ち仕事でむくみっぱなし…<br>
-		下半身太りが気になり始めた</p>
-	<p>そのようなお悩みはありませんか？<br>
-		joieではそのようなお悩みを解決するお手伝いをさせて頂いています。<br>
-		すっきり<span class="marker">キレイな美脚</span>を目指しましょう！</p>
-</div>
-<div class="index">
-	<p>目次</p>
-	<ol>
-		<a href="#1">
-			<li>こんなお悩みありませんか？</li>
-		</a>
-		<a href="#2">
-			<li>むくみの原因</li>
-		</a>
-		<a href="#3">
-			<li>Joieの特徴は</li>
-		</a>
-		<a href="#4">
-			<li>こんな効果が期待できます</li>
-		</a>
-		<a href="#5">
-			<li>施術の流れ</li>
-		</a>
-		<a href="#6">
-			<li>お客様の声</li>
-		</a>
-		<a href="#7">
-			<li>施術料金</li>
-		</a>
-		<a href="#8">
-			<li>最後に</li>
-		</a>
-	</ol>
-</div>
+<section id="intro">
+	<h2>健康的で美しい足に<br class="sp">なりたい方へ</h2>
+	<div>
+		<p>美しい足に憧れがあるけれど、どうしたらなれるか分からない<br>
+			デスクワークや立ち仕事でむくみっぱなし…<br>
+			下半身太りが気になり始めた</p>
+		<p>そのようなお悩みはありませんか？<br>
+			joieではそのようなお悩みを解決するお手伝いをさせて頂いています。<br>
+			すっきり<span class="marker">キレイな美脚</span>を目指しましょう！</p>
+	</div>
+	<div class="index">
+		<p>目次</p>
+		<ol>
+			<a href="#1">
+				<li>こんなお悩みありませんか？</li>
+			</a>
+			<a href="#2">
+				<li>むくみの原因</li>
+			</a>
+			<a href="#3">
+				<li>Joieの特徴は</li>
+			</a>
+			<a href="#4">
+				<li>こんな効果が期待できます</li>
+			</a>
+			<a href="#5">
+				<li>施術の流れ</li>
+			</a>
+			<a href="#6">
+				<li>お客様の声</li>
+			</a>
+			<a href="#7">
+				<li>施術料金</li>
+			</a>
+			<a href="#8">
+				<li>最後に</li>
+			</a>
+		</ol>
+	</div>
+</section>
 <section id="sec1">
-	<h2>こんなお悩みありませんか？</h2>
-	<ul class="check">
-		<li>むくみが気になる </li>
-		<li>腰のハリが気になる</li>
-		<li>デスクワークで座ってばかり</li>
-		<li>毎日、立ち仕事をしている</li>
-		<li>下半身太り</li>
-		<li>歩いていて、脚が重い感じがする…</li>
-	</ul>
+	<h2>こんなお悩み<br class="sp">ありませんか？</h2>
+	<div class="flex">
+		<ul class="check">
+			<li>むくみが気になる </li>
+			<li>腰のハリが気になる</li>
+			<li>デスクワークで座ってばかり</li>
+			<li>毎日、立ち仕事をしている</li>
+			<li>下半身太り</li>
+			<li>歩いていて、脚が重い感じがする…</li>
+		</ul>
+		<figure><img src="https://joie-group.com/wp-content/uploads/2020/02/nayami.jpg" alt="お悩みリスト"></figure>
+	</div>
 </section>
 <section id="sec2">
 	<h2>むくみの原因</h2>
@@ -61,16 +66,17 @@
 	<div>
 		<p>当サロンでは、丁寧なカウンセリングを行いお悩みを詳しくお聞きし、骨盤の歪み等をチェックした上で施術に入ります。<br>
 			基本は<span class="marker">「リンパ・筋肉・骨格」</span>の３つにアプローチ。</p>
-		<p>具体的には、<span class="marker">関節や骨格の矯正、臀筋のマッサージ、腰回り・おしり回りのハリをほぐす</span>ことを中心にアプローチしていきます。<br>
-			矯正といっても<span class="marker">痛みのほとんどない施術</span>なのでご安心ください。<br>
+		<p>具体的には、<span class="marker">関節や骨格の矯正、臀筋のマッサージ、腰回り・おしり回りのハリをほぐす</span>ことを中心にアプローチしていきます。</p>
+		<figure><img src="https://joie-group.com/wp-content/uploads/2020/02/sejyutu1.jpg" alt="joieの特徴" /></figure>
+		<p> 矯正といっても<span class="marker">痛みのほとんどない施術</span>なのでご安心ください。<br>
 			リラクゼーションサロンのようなマッサージの時間はほとんどありませんが、比較的短時間で効果を実感していただけます。</p>
 		<p>手で触ったり目で見てわかるくらいの違いを実感している方がほとんどです。<br>
-			※個人差があります 
+			※個人差があります <br>
 			また、脚・腰の軸を矯正するので、外面だけでなく<span class="marker">身体の内側から根本的な改善</span>が望めます。</p>
 	</div>
 </section>
 <section id="sec4">
-	<h2>こんな効果が期待できます</h2>
+	<h2>こんな効果が<br class="sp">期待できます</h2>
 	<div>
 		<p><span class="marker">脚のむくみがとれる</span>ことはもちろん、<span class="marker">足や腰の疲れの軽減</span>や<span class="marker">ヒップアップ</span>にもつながります。<br>
 			全体的にスタイルがよくなり、<span class="marker">内側からも健康的な身体に</span>近づけます。</p>
@@ -86,37 +92,45 @@
 			<li>下半身のラインがキレイになる</li>
 		</ul>
 	</div>
+	<figure><img src="https://joie-group.com/wp-content/uploads/2020/02/kouka4.jpg" alt="joieの施術で期待できる効果"></figure>
 </section>
 <section id="sec5">
 	<h2>施術の流れ</h2>
-	<p>美脚コース（美体・美脚プロポーション矯正）</p>		
+	<p>美脚コース（美体・美脚プロポーション矯正）</p>
 	<dl>
 		<dt>カウンセリング</dt>
 		<dd>丁寧なカウンセリングを行いお悩みを詳しくお聞きし、骨盤の歪み等をチェックした上で施術に入ります。</dd>
 		<dt>仰向け</dt>
 		<dd>①足関節の矯正<br>
-			②股関節の矯正</dd>
+			②股関節の矯正
+			<figure><img src="https://joie-group.com/wp-content/uploads/2020/02/sejyutu2-300x225.jpg" alt="カウンセリング"></figure>
+		</dd>
 		<dt>横向き</dt>
 		<dd>①臀筋のマッサージ<br>
-			②股関節の矯正</dd>
+			②股関節の矯正
+			<figure><img src="https://joie-group.com/wp-content/uploads/2020/02/sejyutu3-300x225.jpg" alt="横向き"></figure>
+		</dd>
 		<dt>うつ伏せ</dt>
 		<dd>①下腿の矯正<br>
 			②臀筋のマッサージ<br>
-			③股関節の矯正</dd>
+			③股関節の矯正
+			<figure><img src="https://joie-group.com/wp-content/uploads/2020/02/sejyutu5-300x225.jpg" alt="うつ伏せ"></figure>
+		</dd>
+		<dt>最後に手で触って変化を実感！</dt>
 	</dl>
-	<p>最後に手で触って変化を実感！</p>
 </section>
 <section id="sec6">
 	<h2>お客様の声</h2>
 	<p> 短時間で驚くほどの効果があり、ビックリしました。<br>
 		触るだけでも実感しましたし、タイツを履いた時にゆるく感じました。<br>
 		説明も分かりやすく、腰周りのハリが脚のむくみや痛みの原因だったことが分かりました！<br>
+		イタ気持ちいい施術で「効いている～！」という感じがしましたよ。<br>
 		リラックスできる空間も良かったです。<br>
 	</p>
 </section>
 <section id="sec7">
 	<h2>施術料金</h2>
-	<p>美脚コース（美体・美脚プロポーション矯正）7,000円</p>
+	<p>美脚コース（美体・美脚プロポーション矯正）4,000円</p>
 </section>
 <section id="sec8">
 	<h2>最後に</h2>
